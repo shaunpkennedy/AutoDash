4b7ab864651dcef2a3639a635451906d633461b5
Shaun P. Kennedy <shaun@shaunpkennedy.com>
2017-02-06 21:13:10 -0600

    Update routes for home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
-  get 'home/about'
+  
+  get "/about", to: 'home#about', as: 'about'
+  root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
